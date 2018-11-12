e372a841441226ea79d0d1445543119a6c731c1a
Manuel Hinz <csidestep@gmail.com>
2018-11-12 23:41:01 +0100

    updated Stuff

--- DataCollection/data_collection.py
+++ DataCollection/data_collection.py
@@ -1,8 +1,16 @@
 import riotapi
-
+from riot_classes import Account
+from riot_constants import EUW, NA
 def main():
     rapi = riotapi.RiotApi()
-    rapi.
+    
+def build_data_base(inital_acc:Account, active_players:int, rapi:riotapi.RiotApi):
+    accs = []
+    while len(accs) < active_players:
+        rapi.get_matchlist()
+
+def append_data_base():
+    pass
 
 def loop():
     pass
--- DataCollection/riotapi.py
+++ DataCollection/riotapi.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 from riot_constants import EUW, NA
 from riot_classes import Account
 
@@ -24,18 +25,36 @@ class RiotApi:
     def get_rank(self, acc:Account,soloq:bool=True):
         json = requests.get(f"https://{acc.region}.api.riotgames.com/lol/league/v3/positions/by-summoner/{acc.id}?api_key={self.key}").json()
         for queue in json:
-            if queue["queueType"] == "RANKED_SOLO_5x5":
-                return queue["tier"] + " " + queue["rank"]
+            if soloq:
+                if queue["queueType"] == "RANKED_SOLO_5x5":
+                    return queue["tier"] + " " + queue["rank"]
+            else:
+                if queue["queueType"] == "RANKED_FLEX_SR":
+                    return queue["tier"] + " " + queue["rank"]
         return "unranked I"
 
-    def get_matchlist(self):
-        pass
+    def get_matchlist(self, acc:Account, number_of_games:int):
+        json = requests.get(f"https://{acc.region}.api.riotgames.com/lol/match/v3/matchlists/by-account/{acc.acc_id}?api_key={self.key}").json()
+        matches = json["matches"]
+        while len(matches) < number_of_games:
+            json2 = requests.get(f"https://{acc.region}.api.riotgames.com/lol/match/v3/matchlists/by-account/{acc.acc_id}?beginIndex={len(matches)}&api_key={self.key}").json()
+            matches.extend(json2["matches"])
+        print(matches[len(matches)-1])
+        return matches
+    
+    def get_match(self, id:int, region:str=EUW):
+        json = requests.get(f"https://{region}.api.riotgames.com/lol/match/v3/matches/{id}?api_key={self.key}").json()
+        return json
+
+    def get_players_by_match(self, id:int, region:str=EUW):
+        self.get_match(id, region)
 
 if __name__ == "__main__":
     rapi = RiotApi()
     rapi.update_credentials()
     rapi.print_key() 
     acc1 = rapi.get_acc_by_name("C SideStep", EUW)
-    acc2 = rapi.get_acc_by_name("Alien Mazort", EUW)
-    print(rapi.get_rank(acc1))
-    print(rapi.get_rank(acc2))
+    #ts = rapi.get_matchlist(acc1, 500)[0]["timestamp"]
+    #ts = str(ts-3628800000)
+    #print(datetime.fromtimestamp(int(str(ts)[:-3])))
+    print(rapi.get_match(3758727935, EUW))
